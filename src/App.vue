e8e7fda4822e73f65d2014b9e2bd775595c154da
lucastvasconcelos <lucastavaresvasconcelos@gmail.com>
2017-03-17 17:37:18 -0300

    adicionando aplicacao, com filtro e component cadastro ja completo

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+   <div class="corpo">
+   xiu
+   
+   </div>
+   </div>
+</template>
+
+<script>
+export default {
+      
+}
+</script>
+
+<style>
+    .corpo{
+        font-family : Helvetica,sans-serif;
+        margin: 0 auto;
+        width: 96%;
+    }  
+</style>
---
+++ src/cadastro/Cadastro.vue
@@ -0,0 +1,14 @@
+<template>
+    <h2 class="centralizado">
+        Cadastro de fotos
+    </h2>
+</template>
+<script>
+    
+</script>
+
+<style>
+    .cadastro{
+        text-align: center;
+    }
+</style>
